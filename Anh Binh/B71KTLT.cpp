f70966dbceb3cd3df08ad98e392b651951c09a3f
Binh0807 <binhanh2266@gmail.com>
2025-11-19 19:58:03 +0700

    Update B71KTLT.cpp

--- Anh Binh/B71KTLT.cpp
+++ Anh Binh/B71KTLT.cpp
@@ -15,6 +15,15 @@ void xuat(int a[], int n, char ten){
     printf("\n");
 }
 
+int giamdan(int a[], int n){
+    for(int i = 0; i < n - 1; i++){
+        if(a[i] <= a[i + 1]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void chenmang(int b[], int *kichthuoc, int m){
     int i = *kichthuoc - 1;
     while(i >= 0 && b[i] < m){
@@ -37,21 +46,25 @@ int main(){
     scanf("%d", &n);
     printf("Nhap kich thuoc mang B: ");
     scanf("%d", &m);
-    
     int a[n], b[m + n];
-    
     nhap(a, n, 'A');
     nhap(b, m, 'B');
-    
     printf("Truoc khi noi:\n");
     xuat(a, n, 'A');
     xuat(b, m, 'B');
-    
+    if(giamdan(b, m==1)){
+        printf("Mang B la mang giam dan.\n");
+    } else {
+        printf("Mang B KHONG phai la mang giam dan.\n");
+    }
     int m1 = m;
     noimang(a, b, &m1, n);
-    
     printf("Sau khi noi:\n");
     xuat(b, m1, 'B');
-    
+    if(kiemtragiamdan(b, m1)){
+        printf("Mang B sau khi noi la mang giam dan.\n");
+    } else {
+        printf("Mang B sau khi noi KHONG phai la mang giam dan.\n");
+    }
     return 0;
 }
