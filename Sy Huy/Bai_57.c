dd6504cd9cf1616d984a4d533d901e5ce4b0a920
Hồ Sỹ Huy <hosyhuy29@gmail.com>
2025-12-02 07:56:52 +0700

    Update Bai_57.c

--- Sy Huy/Bai_57.c
+++ Sy Huy/Bai_57.c
@@ -2,7 +2,7 @@
 
 int so_hoan_hao(int n) {
     if (n <= 1) return 0;
-    int sum = 1;                             // 1 luôn là ước
+    int sum = 1;                     
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
             sum += i;
