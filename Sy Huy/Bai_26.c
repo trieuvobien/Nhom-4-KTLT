c332091529a61bd9a544921e52e544e56356cdc6
Hồ Sỹ Huy <hosyhuy29@gmail.com>
2025-12-02 07:51:15 +0700

    Update Bai_26.c

--- Sy Huy/Bai_26.c
+++ Sy Huy/Bai_26.c
@@ -3,21 +3,23 @@
 
 int main() {
     double x;
-    printf("nhap vao so thuc x");
+    printf("nhap vao so thuc x: ");
     scanf("%lf", &x);
     
     double ketQua = 1.0;
-
+    
     for(int k = 1; k <= 32; k++) {
-        double tu = x - 2.0 * k;
-        double mau = x - 2.0 * k - 1.0;
+        double soChan = x - 2.0 * k;
+        double soLe   = x - 2.0 * k - 1.0;
         
         if(fabs(soLe) < 1e-12) {
             printf("Mau so bang 0\n");
             return 0;
         }
-        ketQua *= tu / mau;
+        
+        ketQua *= soChan / soLe;
     }
+    
     printf("%.10f\n", ketQua);
     return 0;
 }
