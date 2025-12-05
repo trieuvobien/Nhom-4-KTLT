5e1f0b13239c0b30773a223c3e2cb147d917a30e
Cá Sợ Nước <103564073+Cassonuoc@users.noreply.github.com>
2025-12-05 23:33:21 +0700

    bai54.c

--- Dung Dam/bai54.c
+++ Dung Dam/bai54.c
@@ -1,30 +1,51 @@
 #include<stdio.h>
-int n,c;
+#include<math.h>
+double n;
+int c;
 
-void dcs(int a, int b) 
+void dcs(double a, int b)
 {
-	char d[200];
+	char d[300];
 	char cs[]="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int j=0;
+	int i=0;
 	if(a==0)
 	{
 		printf("0");
 		return;
 	}
-	while(a>0)
+	long long c=(long long)a;
+	a-=c;
+	while(c>0)
 	{
-		d[j]=cs[a%b];
-		j++;
-		a=a/b;
+		d[i]=cs[c%b];
+		i++;
+		c/=b;
 	}
-	for(j=j-1;j>=0;j--)
-		printf("%c",d[j]);
+	for(i=i-1;i>=0;i--)
+		printf("%c",d[i]);
+	if(a==0) return;
+	else
+	{
+		printf(".");
+		i=10;
+		while((a>0) && (i>0))
+		{
+			a*=b;
+			printf("%c",cs[(int)a]);
+			a-=(int)a;
+			i--;
+		}
+	}
 }
-
 int main()
 {
-	scanf("%d",&n);
-	scanf("%d",&c);
-	dcs(n,c);
+	scanf("%lf %d",&n,&c);
+	while(c<2 || c>36)
+	{
+		printf("Vui long nhap lai co so muon doi");
+		scanf("%d",&c);
+	}
+	if(n<0) printf("-");
+	dcs(fabs(n),c);
 	return 0;
 }
